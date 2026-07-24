89b4b001f45d680a5d59fb79561e14c348ea9511
PamelaSay <147214129+PamelaSay@users.noreply.github.com>
2026-07-24 20:40:42 -0300

    Update planodecorte.js

--- planodecorte.js
+++ planodecorte.js
@@ -200,7 +200,7 @@ function atualizarTabelaPecas() {
             <td>${peca.nome}</td>
             <td>${peca.altura}</td>
             <td>${peca.largura}</td>
-            <td>${textoSentido}</td>
+            <td>${texto.sentido}</td>
             <td>${peca.quantidade}</td>
             <td>
                 <button type="button" class="btn-editar" data-index="${index}" title="Editar"><i class="fa-solid fa-pen"></i></button>
